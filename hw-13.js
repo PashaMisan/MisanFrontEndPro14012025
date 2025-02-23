4cda1ef7c764ec37a6902dec2695ebab02ea26c6
PashaMisan <redmi2.misan@gmail.com>
2025-02-23 12:04:03 +0200

    Homework 13

---
+++ hw-13.js
@@ -0,0 +1,17 @@
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+
+const key = generateKey(16, characters);
+
+console.log(key);
