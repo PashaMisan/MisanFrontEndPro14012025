071e6bab9af7dafe58f6a2cf4530b2595fac8e61
PashaMisan <redmi2.misan@gmail.com>
2025-03-16 18:44:31 +0200

    Homework 18

---
+++ hw-18.js
@@ -0,0 +1,18 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+
+        return this;
+    },
+    down: function () {
+        this.step--;
+
+        return this;
+    },
+    showStep: function () { // показує теперішню сходинку
+        alert(this.step);
+    }
+};
+
+ladder.up().up().down().showStep(); // 1
