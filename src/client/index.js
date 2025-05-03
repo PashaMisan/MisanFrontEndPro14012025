42b74871b002ff947ea7e80ef76de26141b93cd7
PashaMisan <redmi2.misan@gmail.com>
2025-05-03 20:08:50 +0300

    Homework 37 base config

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+import './styles.scss';
+
+console.log('hello')
+console.log('hell3o')
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+        template: "./src/client/index.html"
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
